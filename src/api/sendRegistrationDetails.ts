3fada221a662cd7dcf8e693033dde7e0741ae446
Grisha Strizjnev <grishastrizjnev@MacBook-Pro-Grisha.local>
2023-10-02 19:37:58 +0300

    add API url

--- src/api/sendRegistrationDetails.ts
+++ src/api/sendRegistrationDetails.ts
@@ -1,5 +1,7 @@
+import { config } from '@/config'
+
 export const sendRegistrationDetails = async() => {
-    const response = await fetch('')
+    const response = await fetch(`${config.BASE_URL}/test`)
     const data = await response.json()
     return data
 }
