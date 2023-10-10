2951efac1eff232231a0b5f756758485833baac4
Grisha Strizjnev <grishastrizjnev@gmail.com>
2023-10-10 18:16:28 +0300

    feat: database sql

--- database/users.sql
+++ database/users.sql
@@ -1,8 +1,9 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY, 
     first_name VARCHAR(255) NOT NULL,
-    sub_name VARCHAR(255) NOT NULL,
+    sub_name VARCHAR(255) NULL,
     email VARCHAR(255) NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
-    token VARCHAR(255) NOT NULL
+    token VARCHAR(255) NULL
+    my-quizzes INTEGER[] NULL
 );
