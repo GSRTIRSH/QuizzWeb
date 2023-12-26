736875fcb9a47ff18f7a37defd6e34bc73725183
Grisha Strizjnev <grishastrizjnev@gmail.com>
2023-12-27 01:09:14 +0300

    feat: docker compose all

--- database/users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY, 
-    first_name VARCHAR(255) NOT NULL,
-    sub_name VARCHAR(255) NULL,
-    email VARCHAR(255) NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
-    token VARCHAR(255) NULL,
-    my_quizzes INTEGER[] NULL
-);
